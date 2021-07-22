c264a721b09548845686c46cfc46d0242cc29d35
Chunkie0 <davisdambis1@gmail.com>
2021-07-22 21:38:01 +0300

    redesigned look

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-  title: string = 'Weather Forecast'
+  title: string = 'Weather Info'
   constructor() { }
 
   ngOnInit(): void {
